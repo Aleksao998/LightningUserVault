0d1fbcf434a57433e00dd00cea63b88a61542426
Aleksao998 <aleksaopacic988@gmail.com>
2023-09-09 23:09:54 +0200

    core/support_env_vars

--- core/command/helper/helper_test.go
+++ core/command/helper/helper_test.go
@@ -1,8 +1,9 @@
 package helper
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestResolveAddr(t *testing.T) {
--- core/main.go
+++ core/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/Aleksao998/LightingUserVault/core/command/root"
+import (
+	"github.com/Aleksao998/LightingUserVault/core/command/root"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	// if .env does not exist, cobra commands will be used
+	godotenv.Load()
+
 	root.NewRootCommand().Execute()
 }
